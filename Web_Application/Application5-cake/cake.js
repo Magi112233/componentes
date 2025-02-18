fbbbe259c3532a4a44eefca16404f95379bd2ad5
Magi112233 <mageswarimagi07@gmail.com>
2025-02-18 09:11:30 -0800

    h

--- Web_Application/Application5-cake/cake.js
+++ Web_Application/Application5-cake/cake.js
@@ -3,4 +3,13 @@ const searchForm = document.querySelector(".search-form");
 
 searchIcon.addEventListener("click", () => {
   searchForm.classList.add("active");
+  cartItemContainer.classList.remove("active");
+});
+
+const cartIcon = document.querySelector(".cart-icon");
+const cartItemContainer = document.querySelector(".cart-items-container");
+
+cartIcon.addEventListener("click", () => {
+  cartItemContainer.classList.add("active");
+  searchForm.classList.remove("active");
 });
